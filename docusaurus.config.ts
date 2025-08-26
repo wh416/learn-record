139ac19e240dde24da2a3a3fd095b729575361b4
wh <wanghua416416@126.com>
2025-08-26 22:30:04 +0800

    i

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Learn Record',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
