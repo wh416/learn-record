896873e87cd2e3a77b3f8cb70997c7cdf469b21f
wh <wanghua416416@126.com>
2025-08-26 22:28:11 +0800

    i

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Learn Record',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
